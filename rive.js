1c03ed2cf55ad7ad5714041740d418455b667a82
MBP27 <MBP27@mac.home>
2025-06-26 11:36:17 +0200

    add basic setup

---
+++ rive.js
@@ -0,0 +1,25 @@
+const stateMachine = "State Machine 1";
+
+const riveInstance = new rive.Rive({
+	src: "the_arcane-jauge.riv", //get rive file
+	canvas: document.getElementById("rive"), //get correct canvas
+	autoplay: true,
+	stateMachines: stateMachine, // get correct stateMachine
+	automaticallyHandleEvents: true, // Automatically handle RiveHTTPEvents
+	onLoad: onLoadHandler,
+});
+
+// Resize the drawing surface if the window resizes
+window.addEventListener(
+	"resize",
+	() => {
+		riveInstance.resizeDrawingSurfaceToCanvas();
+	},
+	false
+);
+
+function onLoadHandler() {}
+
+const eventFire = (riveEvent) => {};
+// Register the event handler
+riveInstance.on(rive.EventType.RiveEvent, eventFire);
